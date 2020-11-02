0738f6add8971f9bed985e1643346e2c90395d8f
Szymon Chomej <schomej@jaworski.local>
2020-11-02 13:02:12 +0100

    My song

--- Kurs Pirple/Homeworks/main.py
+++ Kurs Pirple/Homeworks/main.py
@@ -1,5 +1,19 @@
 """
-Szymon's favourite song parameters
+Szymon's Chomej
+favourite song parameters
 """
+artist = "Kwiat Jabłoni"   #Best Polish band :)
+title = "Dziś Późno Pójdę Spać"
+genre = "Alternative"
+album = "Niemożliwe"
+originCountry = "Poland"
+releaseYear = 2019
+durationsInSeconds = 232
+youTubeViews = 22223205
+mySonLikeIt = True
 
-genre = "Pop"
+"""
+My score in scale 1:10. 
+When 1 is the worst and 10 is the best
+"""
+myScore = 9,5

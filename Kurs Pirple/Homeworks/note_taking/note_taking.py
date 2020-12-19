bd0866315f9d31c2f5410870971c35a4a2809892
Szymon Chomej <schomej@jaworski.local>
2020-12-19 12:40:23 +0100

    Note-taking program homework done

--- Kurs Pirple/Homeworks/note_taking/note_taking.py
+++ Kurs Pirple/Homeworks/note_taking/note_taking.py
@@ -4,6 +4,7 @@ Created on Thu Dec 17 08:29:10 2020
 
 @author: schomej
 """
+import os
 import os.path
 from os import path
 
@@ -18,24 +19,47 @@ while(True):
     isFileThere = path.isfile(fileName)
 
     if isFileThere == False:
-        textToWrite = str(input("Please enter text to write to the file: \n"))
+        textToWrite = str(input("Please enter text to write to the file: \n")) + "\n"
         notesFile = open(fileName,"w")
         notesFile.write(textToWrite)
         notesFile.close()
         break
     if isFileThere == True:
-        action = str(input("What next:\n r - read file \n d - delete file \n a - append to file \n x - exit \n"))
+        action = str(input("What next:\n r - read file \n d - delete file \n a - append to file \n c - change single line \n x - exit \n"))
         if action == "x":
             break
         else:
+            notesFile = open(fileName,"r+")
             if action == "r":
-                notesFile = open(fileName,"r")
                 readFile(notesFile)
-                notesFile.close()
             if action == "a":
-                notesFile = open(fileName,"r+")
                 text = str(input("Enter the text which will be added to the file: \n")) + "\n"
                 notesFile.write(text)
                 readFile(notesFile)
-                notesFile.close()
-            
+            if action == "c":
+                lineNumber = int(input("Which line number do you want to change: "))
+                lineNumber -= 1
+                newText = str(input("Now enter the new text: ")) + "\n"
+                
+                fileContentByLines = notesFile.readlines()
+                contentLineNumbers = len(fileContentByLines)
+                
+                while (True):
+                    if lineNumber <= contentLineNumbers:
+                        fileContentByLines[lineNumber] = newText
+                        break
+                    else:
+                        lineNumber = int(input("Your number line is out of range, enter new number: "))
+                        lineNumber -= 1
+        
+                notesFile.truncate(0)
+                
+                for line in fileContentByLines:
+                    notesFile.write(line)
+                    
+                readFile(notesFile)
+                
+            if action == "d":
+                os.remove(fileName)
+             
+            notesFile.close()

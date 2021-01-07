18478c82e988310a5bdfe1db8643ff3e787caf55
Web-Control <szymon@web-control.pl>
2021-01-07 22:24:51 +0100

    Hangman The Game: hangman drawed, english words added

--- Kurs Pirple/Homeworks/Hangman The Game/hangman_the_game.py
+++ Kurs Pirple/Homeworks/Hangman The Game/hangman_the_game.py
@@ -4,6 +4,7 @@ Created on Wed Jan  6 20:52:14 2021
 
 @author: psnma
 """
+from random import randint
 
 #HANGMAN THE GAME
 
@@ -28,14 +29,48 @@ def drawHangman(parts):
     torso = "     |"
     legs = "    //"
     
-    print(topBar)
-    print(topPost)
-    print(pieceOfPost + head)
-    print(pieceOfPost + shoulders)
-    print(pieceOfPost + torso)
-    print(pieceOfPost + legs)
-    print(bottomBar)
+    if parts[0] == True:
+        print(topBar)
+        
+    if parts[1] == True:    
+        print(topPost)
+        
+    if parts[2] == True and parts[7] == True:
+        print(pieceOfPost + head)
+    elif parts[2] == True:
+        print(pieceOfPost)
+        
+    if parts[3] == True and parts[8] == True:
+        print(pieceOfPost + shoulders)
+    elif parts[3] == True:
+        print(pieceOfPost)
+    
+    if parts[4] == True and parts[9] == True:
+        print(pieceOfPost + torso)
+    elif parts[4] == True:
+        print(pieceOfPost)
+    
+    if parts[5] == True and parts[10]:
+        print(pieceOfPost + legs)
+    elif parts[5] == True:
+        print(pieceOfPost)
+    
+    if parts[6] == True:
+        print(bottomBar)
 
-hangmanParts = []
-drawHangman(hangmanParts)
+
+englishWords = []
+
+#Import english words
+with open("mix_of_english_words.txt","r",encoding='utf-8',) as fileWithWords:
+    for word in fileWithWords:
+        englishWords.append(word.strip("\n"))
+        
+
+randomNumber = randint(0, 61000)
+
+hangmanParts = [True,True,True,True,True,True,True,False,False,False,False]
+#drawHangman(hangmanParts)
+
+
     

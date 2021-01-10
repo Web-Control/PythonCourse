1250e1c661c6d5f8e747c5d882a2eef2ea2d1dc0
Web-Control <szymon@web-control.pl>
2021-01-10 21:11:20 +0100

    Hangman the game - one player mode finished

--- Kurs Pirple/Homeworks/Hangman The Game/hangman_the_game.py
+++ Kurs Pirple/Homeworks/Hangman The Game/hangman_the_game.py
@@ -58,19 +58,96 @@ def drawHangman(parts):
     if parts[6] == True:
         print(bottomBar)
 
+def setActualWord(letterPlaces,letter):
+    for place in letterPlaces:
+        actualWord[place-1] = letter
+    
 
+def drawLettersSpaces(word):
+    for letter in word:
+        print(letter+" ", end = "")
+        
+def checkIfWordHaveLetter(word,letterToFind):
+    wordLength = len(word)
+    letterPlaces = []
+    x = 1
+    for letter in word:
+        if letter == letterToFind:
+            letterPlaces.append(x)
+        
+        placesAmount = len(letterPlaces)
+            
+        if x == wordLength and placesAmount > 0:
+            return letterPlaces
+            break
+        elif x == wordLength and placesAmount == 0:
+            return False
+        x += 1
+        
 englishWords = []
 
 #Import english words
 with open("mix_of_english_words.txt","r",encoding='utf-8',) as fileWithWords:
     for word in fileWithWords:
         englishWords.append(word.strip("\n"))
-        
-
+ 
+#Initial data
 randomNumber = randint(0, 61000)
+#wordToGuess = englishWords[randomNumber]
+wordToGuess = "playstation"
+wordLength = len(wordToGuess)
 
-hangmanParts = [True,True,True,True,True,True,True,False,False,False,False]
-#drawHangman(hangmanParts)
-
-
+#Lets play :)
+while(True):
+    hangmanParts = [True,True,True,True,True,True,True,False,False,False,False]
+    partNumberToDraw = 7
+    actualWord = []
+    for letter in wordToGuess:
+        actualWord.append("_")
+    
+    playersNumber = int(input("How many players will play 1 or 2 ?   If you want to exit enter 0. \n"))
+    
+    if playersNumber == 1:
+        while(True):
+            print(" \n")
+            print("GAME FOR ONE PLAYER")
+            
+            drawHangman(hangmanParts)
+            
+            #Checking if player loose
+            if hangmanParts[10] == True:
+                print(" \n")
+                print("YOU LOOSE !!!")
+                break
+            
+            print(" \n")
+            print("Word to guess:")
+            drawLettersSpaces(actualWord)
+            print(" \n")
+            
+            #Checking if playe won
+            if actualWord.count("_") == 0:
+                print("YOU WIN !!!")
+                break
+            
+            enteredLetter = input("Enter your letter: ")
+            isLetterInWord = checkIfWordHaveLetter(wordToGuess,enteredLetter)
+            
+            #Check if word contain letter
+            if isLetterInWord != False:
+                setActualWord(isLetterInWord,enteredLetter)
+            else:
+                hangmanParts[partNumberToDraw] = True
+                partNumberToDraw += 1
+        
+    elif playersNumber == 2:
+        print("Two players.")
+    elif playersNumber == 0:
+        print(" \n")
+        print("Hope see you soon. Good By.")
+        break
+    else:
+        print(" \n")
+        print("No, no, no :). Try again !!! \n")
+    
     
